893867eb1f66469a3213a0c4d003da7ee6bb6e7e
Ajay Chundi <66492476+asvch@users.noreply.github.com>
2025-03-27 20:49:50 -0400

    Working with hardcoded values for now

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import { CardTitleExample } from './App';
 import reportWebVitals from './reportWebVitals';
+// import '@coreui/coreui/dist/css/coreui.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <CardTitleExample /> */}
   </React.StrictMode>
 );
 
